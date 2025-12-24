a1cd1474c899e3dc2cd7906d5a3ddde9bb588021
N <persaudnirav@gmail.com>
2025-12-24 11:24:13 -0500

    feat: connect api to frontend

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ function myFunction() {
 function goBack(){
    window.location.href = 'index.html';
 }
+function getData(){
+fetch('https://picsum.photos/v2/list')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch( error => console.error(error));
+}

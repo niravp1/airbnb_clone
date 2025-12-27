43f99b900d0c883299477f8b32f985c03e90e3b0
N <persaudnirav@gmail.com>
2025-12-26 20:05:01 -0500

    feat: render card for image

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 function myFunction() {
    window.location.href = 'test.html';
 }
+
 function goBack() {
    window.location.href = 'index.html';
 }
+
 async function getData() {
-   response = await fetch('https://picsum.photos/v2/list')
-   data = await response.json()
-   // use for loop to render 5 images then style them ; dynamically create new elements to do so
-   for (let i = 0; i < 5; i++){
-      loadImage(data[i].download_url)
-   } }
-function loadImage(url){
-   document.getElementById("test-image").setAttribute("src",url)
+   const response = await fetch('https://picsum.photos/v2/list');
+   const data = await response.json();
+
+   const gallery = document.getElementById("gallery");
+
+   for (let i = 0; i < 5; i++) {
+      createCard(data[i], gallery);
+   }
+}
+
+function createCard(imageData, container) {
+   const card = document.createElement("div");
+   card.className = "card";
+
+   const img = document.createElement("img");
+   img.src = imageData.download_url;
+   img.className = "card-image";
+
+   // optional text
+   const caption = document.createElement("p");
+   caption.textContent = imageData.author;
+   caption.className = "card-text";
+
+   card.appendChild(img);
+   card.appendChild(caption);
+   container.appendChild(card);
 }

d167484f240381dc3007707db4cb9c83233e7a05
N <persaudnirav@gmail.com>
2026-01-03 11:27:45 -0500

    feat: render image through function works

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
+const grid = document.getElementById("grid-display");
 function myFunction() {
-   window.location.href = 'test.html';
+  window.location.href = "test.html";
 }
-function goBack(){
-   window.location.href = 'index.html';
+
+function goBack() {
+  window.location.href = "index.html";
 }
-function getData(){
-fetch('https://picsum.photos/v2/list')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch( error => console.error(error));
+
+async function getData() {
+  const response = await fetch("https://picsum.photos/v2/list");
+  const data = await response.json();
+  renderCard(data[0]);
+}
+function renderCard(data) {
+  const card = document.createElement("div");
+  const image = document.createElement("img");
+  image.src = data.download_url;
+  card.appendChild(image);
+  grid.appendChild(card);
 }

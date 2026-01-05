74050d6863c6aecd92766fbff03edd8f878caba1
N <persaudnirav@gmail.com>
2026-01-04 23:57:38 -0500

    chore: add styling for card

--- app.js
+++ app.js
@@ -10,12 +10,14 @@ function goBack() {
 async function getData() {
   const response = await fetch("https://picsum.photos/v2/list");
   const data = await response.json();
-  renderCard(data[0]);
+  renderCard(data[1]);
 }
 function renderCard(data) {
   const card = document.createElement("div");
   const image = document.createElement("img");
   image.src = data.download_url;
+  image.className = "w-full h-64"
   card.appendChild(image);
   grid.appendChild(card);
+  
 }
